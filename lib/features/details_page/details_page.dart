b5776eea21da564969e954ef110f749ac8ded39b
DrownInred <jonelalibasa@gmail.com>
2022-09-01 01:32:18 -0700

    Details Page Fixes Again

--- lib/features/details_page/details_page.dart
+++ lib/features/details_page/details_page.dart
@@ -95,11 +95,7 @@ class _ContentDetailsPageState extends State<ContentDetailsPage> {
                             width: 295,
                             child: Container(
                               margin: const EdgeInsets.only(
-                                top: 0,
-                                bottom: 0,
-                                right: 16,
-                                left: 0,
-                              ),
+                                  top: 0, bottom: 0, right: 16, left: 0),
                               color: Colors.white,
                             ),
                           ),
@@ -140,85 +136,92 @@ class _ContentDetailsPageState extends State<ContentDetailsPage> {
                   child: Column(
                     children: [
                       Container(
-                        margin: const EdgeInsets.all(4),
+                        margin: const EdgeInsets.only(
+                            top: 8, bottom: 0, right: 0, left: 0),
                         padding: const EdgeInsets.all(4),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(8),
                           color: Colors.grey[850],
                         ),
                         height: 50,
-                        child: Row(
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: Color(0xFF011530),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(2),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(4),
+                                      color: Color(0xFF011530),
+                                    ),
+                                    height: 40,
+                                    width: 40,
                                   ),
-                                  height: 40,
-                                  width: 40,
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      top: 0, bottom: 0, right: 36, left: 4),
-                                  child: Text(
-                                    'Booking',
-                                    style: TextStyle(
-                                      color: Colors.white,
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        top: 0, bottom: 0, right: 38, left: 4),
+                                    child: Text(
+                                      'Booking',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: Color(0xFF011530),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(2),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(4),
+                                      color: Color(0xFF011530),
+                                    ),
+                                    height: 40,
+                                    width: 40,
                                   ),
-                                  height: 40,
-                                  width: 40,
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      top: 0, bottom: 0, right: 36, left: 4),
-                                  child: Text(
-                                    'Route Place',
-                                    style: TextStyle(
-                                      color: Colors.white,
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        top: 0, bottom: 0, right: 38, left: 4),
+                                    child: Text(
+                                      'Route Place',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: Color(0xFF011530),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(2),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(4),
+                                      color: Color(0xFF011530),
+                                    ),
+                                    height: 40,
+                                    width: 40,
                                   ),
-                                  height: 40,
-                                  width: 40,
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      top: 0, bottom: 0, right: 36, left: 4),
-                                  child: Text(
-                                    'Share Experience',
-                                    style: TextStyle(
-                                      color: Colors.white,
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        top: 0, bottom: 0, right: 38, left: 4),
+                                    child: Text(
+                                      'Share Experience',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            )
-                          ],
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                       Padding(
@@ -227,8 +230,11 @@ class _ContentDetailsPageState extends State<ContentDetailsPage> {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[850],
+                                borderRadius: BorderRadius.circular(12),
+                              ),
                               padding: const EdgeInsets.all(8),
-                              color: Colors.grey[850],
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
@@ -253,6 +259,7 @@ class _ContentDetailsPageState extends State<ContentDetailsPage> {
                                           child: Text(
                                             "The establishment of Bangsamoro was the culmination of several years of peace talks between the Philippine government and several autonomist groups; in particular the Moro Islamic Liberation Front (MILF), which rejected the validity of the ARMM and called for the creation of a region with more powers devolved from the national government. A framework agreement known as the Comprehensive Agreement on the Bangsamoro was negotiated between the Benigno Aquino III administration and the MILF in 2014. After continued negotiations and debates over certain provisions, the Congress of the Philippines created and ratified a basic law for the region, now referred to as the Bangsamoro Organic Law; the bill was signed into law by President Rodrigo Duterte on July 26, 2018. Despite questions on the region's constitutionality, as it would have adopted a parliamentary system in an area of a country with a presidential system of government, no judicial ruling was made against the organic law and consequently the COMELEC held two-part plebiscite: one by ARMM citizens determining whether to dissolve the ARMM and immediately replace it with the Bangsamoro and, following the victory of the yes vote on the first part,[8][9][10] and the second part taken by neighboring municipalities and barangays in the provinces of Lanao del Norte and Cotabato regarding their cession to the Bangsamoro region.[11][12][13][14] As a result of the second part of the plebiscite, 63 barangays of Cotabato province were handed over to the Bangsamoro government, adding to the autonomous region's territory.",
                                             style: TextStyle(
+                                              fontSize: 12,
                                               color: Colors.white,
                                             ),
                                             textAlign: TextAlign.justify,
